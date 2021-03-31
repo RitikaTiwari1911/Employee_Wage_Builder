dd9f62de750ff1333ff4087af49cdf6124f68b6e
RitikaTiwari1911 <spk2ritika1911@gmail.com>
2021-03-31 15:06:49 +0530

    ADDED: Daily wages of the employee

--- Emp_wage.java
+++ Emp_wage.java
@@ -1,17 +1,25 @@
-import java.util.Random;
+import java.util.Random; 
 
 public class Emp_wage {
+    public static void main(String args[]) {
+        Random random = new Random();
+        int ranNum = random.nextInt(2);
+        int EMP_IS_PRESENT = 1;
+        int WAGE_PER_HR = 20;
+        int salary = 0;
+        int FULL_DAY_HR = 8;
 
-    public static void main(String[] args) {
-        {
-            Random random = new Random();
-            int ranNum = random.nextInt(2);
-
-            if (ranNum == 0) {
+        if (ranNum == 0) {
                 System.out.println("emp absent");
             } else {
                 System.out.println("emp present");
             }
+        if (ranNum == EMP_IS_PRESENT) {
+            salary = WAGE_PER_HR * FULL_DAY_HR;
+            System.out.println("salary for the day is " + salary);
+        } else {
+            salary = 0;
+            System.out.println("salary for the day is " + salary);
         }
     }
 }
