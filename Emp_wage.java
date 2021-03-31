0c69e4e1f5e0098cef1afaebc6caefb873d86cb8
RitikaTiwari1911 <spk2ritika1911@gmail.com>
2021-03-31 14:56:06 +0530

    ADDED: emp is present or absent

--- Emp_wage.java
+++ Emp_wage.java
@@ -1,5 +1,17 @@
+import java.util.Random;
+
 public class Emp_wage {
-	public static void main (String args[]){
-		System.out.println("Welcome to the Employee wage Builder Program");
-	}
+
+    public static void main(String[] args) {
+        {
+            Random random = new Random();
+            int ranNum = random.nextInt(2);
+
+            if (ranNum == 0) {
+                System.out.println("emp absent");
+            } else {
+                System.out.println("emp present");
+            }
+        }
+    }
 }
