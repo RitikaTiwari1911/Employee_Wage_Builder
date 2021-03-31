0ad0fc24a03613bd80595f8f27ca8761006958c5
RitikaTiwari1911 <spk2ritika1911@gmail.com>
2021-03-31 15:26:39 +0530

    MODIFIED: used switch case

--- Emp_wage.java
+++ Emp_wage.java
@@ -1,27 +1,30 @@
 import java.util.Random;
+
 public class Emp_wage {
     public static void main(String args[]) {
         Random random = new Random();
         int ranNum = random.nextInt(3);
-        int EMP_IS_PRESENT = 1;
-        int PART_TIME=2;
-        int PART_TIME_HR=4;
+        //int EMP_IS_PRESENT = 1;
+        // int PART_TIME = 2;
+        int PART_TIME_HR = 4;
         int WAGE_PER_HR = 20;
-        int salary = 0;
         int FULL_DAY_HR = 8;
+        int empHr=0;
 
+        switch (ranNum) {
+            case 1:
+                empHr=FULL_DAY_HR;
+                break;
 
-        if (ranNum == EMP_IS_PRESENT) {
-            salary = WAGE_PER_HR * FULL_DAY_HR;
-             }
-        else if(ranNum==PART_TIME) {
-            salary = WAGE_PER_HR * PART_TIME_HR;
-        }
-        else {
-            salary=0;
-        }
-    System.out.println("salary for the day is " + salary);
+            case 2:
+                empHr=PART_TIME_HR;
+                break;
 
+            default:
+                empHr=0;
+                break;
         }
+        int salary = empHr * WAGE_PER_HR;
+        System.out.println("salary for the day is " + salary);
     }
-
+}
