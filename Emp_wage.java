6c2b9e73f269d13fee773bcf54d563f6ca95e30b
RitikaTiwari1911 <spk2ritika1911@gmail.com>
2021-04-06 21:03:20 +0530

    ADDED: ability to save emp wage for each company

--- Emp_wage.java
+++ Emp_wage.java
@@ -1,8 +1,22 @@
 import java.util.Random;
 
 public class Emp_wage {
-
-        public static int calculateEmpWage(String companyName, int wagePerHr, int maxWorkingDays, int maxHr){
+	
+		private final String companyName;
+		private final int wagePerHr;
+		private final int maxWorkingDays;
+		private final int maxHr;
+		private int totalWageOfMonth;
+		
+		public Emp_wage(String companyName, int wagePerHr, int maxWorkingDays, int maxHr)
+		{
+			this.companyName=companyName;
+			this.wagePerHr=wagePerHr;
+			this.maxWorkingDays=maxWorkingDays;
+			this.maxHr=maxHr;
+			
+		}
+        public void calculateEmpWage(){
             Random random = new Random();
             
             //constants
@@ -12,12 +26,12 @@ public class Emp_wage {
             //variables
             int totalWorkingDays = 0;
             int empHr = 0;
-            int totalWageOfMonth = 0;
+            //int totalWageOfMonth = 0;
             int salary=0;
             int totalEmpHr=0;
             
             //Calculation Part
-            while (totalWorkingDays < maxWorkingDays &&  totalEmpHr<= maxHr) {
+            while (totalWorkingDays <= maxWorkingDays &&  totalEmpHr<= maxHr) {
                 totalWorkingDays++;
                 int ranNum = random.nextInt(3);
 
@@ -37,14 +51,22 @@ public class Emp_wage {
                 salary=empHr*wagePerHr;
                 totalWageOfMonth += salary;
                 
-            }
-            
-            System.out.println("salary for the month for Company "+companyName+" is "+ totalWageOfMonth);
-            return totalWageOfMonth;
+            }          
         }
+        
+        public String toString() {
+        	return "Total employee Wage for the Company: "+companyName+ " is: "+totalWageOfMonth ;
+        }
+        
+        
         public static void main(String args[]){
-            calculateEmpWage("Dmart",10,25,150);
-            calculateEmpWage("Reliance",20,20,100);
+            Emp_wage dMart=new Emp_wage("dMart",10,25,150);
+            Emp_wage Reliance=new Emp_wage("Reliance",20,20,100);
+            dMart.calculateEmpWage();
+            System.out.println(dMart);
+            Reliance.calculateEmpWage();
+            System.out.println(Reliance);
+            
         }
     }
 
